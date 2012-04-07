48a0a9ff484411034754c0f968da137651bdf0b8
Kirill <dxkxzx@gmail.com>
2012-04-07 14:39:15 +0400

    pages scope desc

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,5 +2,5 @@ class Page < ActiveRecord::Base
   attr_accessible :text, :notebook_id
   belongs_to :notebook
   has_many :notes, :dependent => :destroy 
-  default_scope :order => 'pages.created_at ASC'
+  default_scope :order => 'pages.created_at DESC'
 end
