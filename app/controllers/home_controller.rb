678e774940acec2c6b7b4dcdea589f5788f55789
Kirill <dxkxzx@gmail.com>
2012-04-07 15:52:15 +0400

    fixed nil class exeption

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
   	@notebook = Notebook.new
     @page = Page.new    
     @projects = Project.order("created_at").page(params[:projects_page]).per(5)
-    @pages = current_researcher.notebook.pages.page(params[:pages_page]).per(5)
+    @pages = current_researcher.notebook.pages.page(params[:pages_page]).per(5) if researcher_signed_in?
     @experiments = Project.order("created_at").page(params[:e_page]).per(5)
     @other_experiments  = Project.order("created_at").page(params[:o_e_page]).per(5)
   end
