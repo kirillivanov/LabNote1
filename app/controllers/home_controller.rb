f3a647243b338aca08ad19e85bb784995b8d8fe7
Kirill <dxkxzx@gmail.com>
2012-04-12 21:10:17 +0400

    add ajax, and basic project graph

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,16 @@
 class HomeController < ApplicationController
   def index
   	@notebook = Notebook.new
-    @page = Page.new    
-    @projects = Project.order("created_at").page(params[:projects_page]).per(5)
+
     if researcher_signed_in?
       if current_researcher.notebook
         @pages = current_researcher.notebook.pages.page(params[:pages_page]).per(5) if current_researcher.notebook.pages
+        @experiments = Project.with_experiments(current_researcher).page(params[:experiments_page]).per(5)
+        @other_experiments = Kaminari.paginate_array(Project.with_other_user_experiments(current_researcher)).page(params[:o_e_page]).per(5)
       end 
     end
-    @experiments = Project.order("created_at").page(params[:e_page]).per(5)
-    @other_experiments  = Project.order("created_at").page(params[:o_e_page]).per(5)
+
+    @page = Page.new    
+    @projects = Project.order("created_at").page(params[:projects_page]).per(5)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,13 @@ class Project < ActiveRecord::Base
   has_many :experiments, :dependent => :destroy 
 
   attr_accessible :name
+
+  def self.with_experiments(current_user)
+    includes(:experiments).where("experiments.researcher_id = ?", current_user)
+        #select { |p| p if p.experiments.count > 0 }
+  end
+
+  def self.with_other_user_experiments(current_user)
+    select { |p| p if p.experiments.count > 0 && !p.researchers.include?(current_user) }
+  end
 end
